ccf8641f44a0bef61e6e3db4805de095c6b9e2c6
Tirumala dixjt Bhuram <tirumala@Tirumalas-MacBook-Pro.local>
2020-09-04 11:27:09 -0700

    Create DE

---
+++ CreateDE.js
@@ -0,0 +1,14 @@
+<script runat="server">
+Platform.Load("core","1.1");
+var deObj = {
+    "CustomerKey" : "demoDE",
+    "Name" : "My Demo DE",
+    "Fields" : [
+      { "Name" : "Field 1", "FieldType" : "Number", "IsPrimaryKey" : true, "IsRequired" : true },
+      { "Name" : "Field 2", "FieldType" : "Text", "MaxLength" : 50 },
+      { "Name" : "Field 3", "FieldType" : "Date", "Ordinal" : 2 }
+    ]
+ };
+
+var myDE = DataExtension.Add(deObj);
+</script>
